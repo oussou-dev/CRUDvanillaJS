9480bbc4ca73aa3c862f86ce3bdeaa0dd6234469
oussou-dev <oussou.dev@gmail.com>
2019-01-29 17:30:36 +0100

    Add function addItem()

--- js/app.js
+++ js/app.js
@@ -1,15 +1,13 @@
-// gets element
-
+// get elements
 const itemForm = document.getElementById("itemForm")
 const itemInput = document.getElementById("itemInput")
-const itemList = document.querySelector(".item-list")
+const itemList = document.querySelector(".list-item")
 const clearBtn = document.getElementById("clearList")
 const feedback = document.querySelector(".feedback")
 
 let itemData = []
 
 // form submission
-
 itemForm.addEventListener("submit", e => {
 	e.preventDefault()
 
@@ -23,7 +21,7 @@ itemForm.addEventListener("submit", e => {
 	}
 })
 
-// feedback funtio
+// feedback function
 showFeedback = (text, action) => {
 	// display feedback
 	feedback.classList.add("showItem", `alert-${action}`)
@@ -32,4 +30,19 @@ showFeedback = (text, action) => {
 	setTimeout(() => {
 		feedback.classList.remove("showItem", `alert-${action}`)
 	}, 1500)
+}
+
+// addItem function
+addItem = value => {
+	const div = document.createElement("div")
+	div.classList.add("item", "my-3")
+	div.innerHTML = `
+		<h6 class="item-name text-capitalize">${value}</h6>
+		<div class="item-icons">
+			<a href="#" class="complete-item mx-2 item-icon"><i class="far fa-check-circle"></i></a>
+			<a href="#" class="edit-item mx-2 item-icon"><i class="far fa-edit"></i></a>
+			<a href="#" class="delete-item item-icon"><i class="far fa-times-circle"></i></a>
+		</div>
+	`
+	itemList.appendChild(div)
 }
